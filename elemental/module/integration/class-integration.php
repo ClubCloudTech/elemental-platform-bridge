bf34a023ea6cf4983f30ff85214b75fd81124ef4
Sergei Starovojtov <sergei@coadute.com>
2022-08-02 11:40:31 +0100

    Sync Engine Employee integration

---
+++ elemental/module/integration/class-integration.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * NMP Integration Package
+ * Managing helper functions for Integration
+ *
+ * @package ElementalPlugin\Module\Integration
+ */
+
+namespace ElementalPlugin\Module\Integration;
+
+use ElementalPlugin\Library\Factory;
+use ElementalPlugin\Library\Ajax;
+
+/**
+ * Class Integration - Main Control Function Class for Integration.
+ */
+class Integration {
+
+	const SETTING_INTEGRATION_API_KEY		= 'setting-integration-api-key';
+	const SETTING_INTEGRATION_API_BASEURL 	= 'setting-integration-api-base-url';
+
+	/**
+	 * Runtime Shortcodes and Setup
+	 * Required for Normal Runtime.
+	 */
+	public function init(): void {
+		\add_filter( 'myvideoroom_maintenance_result_listener', array( $this, 'update_integration_api_key' ), 10, 2 );
+		\add_filter( 'myvideoroom_maintenance_result_listener', array( $this, 'update_integration_api_baseurl' ), 10, 2 );
+
+		\add_filter( 'elemental_page_option', array( $this, 'add_integration_api_key_setting' ), 10, 2 );
+		\add_filter( 'elemental_page_option', array( $this, 'add_integration_api_baseurl_setting' ), 10, 2 );
+	}
+
+	/**
+	 * Process Update Result. Integration API Key
+	 *
+	 * @param array $response -  Inbound response Elements that will go back to the Ajax Script.
+	 * @return array
+	 */
+	public function update_integration_api_key( array $response ): array {
+		$value = Factory::get_instance( Ajax::class )->get_string_parameter( self::SETTING_INTEGRATION_API_KEY );
+		\update_option( self::SETTING_INTEGRATION_API_KEY, $value );
+		$response['feedback'] = \esc_html__( 'Integration API Key Saved', 'myvideoroom' );
+		return $response;
+	}
+
+	/**
+	 * Process Update Result. Integration API Base URL
+	 *
+	 * @param array $response -  Inbound response Elements that will go back to the Ajax Script.
+	 * @return array
+	 */
+	public function update_integration_api_baseurl( array $response ): array {
+		$value = Factory::get_instance( Ajax::class )->get_string_parameter( self::SETTING_INTEGRATION_API_BASEURL );
+		\update_option( self::SETTING_INTEGRATION_API_BASEURL, $value );
+		$response['feedback'] = \esc_html__( 'Integration Base URL Key Saved', 'myvideoroom' );
+		return $response;
+	}
+
+	/**
+	 * Add Integration API Key Setting to Plugin Menu
+	 *
+	 * @param array $input - the filter input.
+	 * @return array
+	 */
+	public function add_integration_api_key_setting( array $input ): array {
+		$input_add = ' 
+		<td>
+		<span>' . esc_html__( 'Integration API Key', 'myvideoroom' ) . '</span>
+		</td>
+		<td>
+		<input type="text" class="mvr-main-button-enabled myvideoroom-maintenance-setting"
+			id="' . esc_attr( self::SETTING_INTEGRATION_API_KEY ) . '" value="' . \get_option( self::SETTING_INTEGRATION_API_KEY ) . '">
+			<i class="myvideoroom-dashicons mvr-icons dashicons-editor-help" title="' . \esc_html__( 'Integration API Key', 'myvideoroom' ) . '"></i>
+		</td>';
+		\array_push( $input, $input_add );
+		return $input;
+	}
+
+	/**
+	 * Add Integration API Base URL Setting to Plugin Menu
+	 *
+	 * @param array $input - the filter input.
+	 * @return array
+	 */
+	public function add_integration_api_baseurl_setting( array $input ): array {
+		$input_add = ' 
+		<td>
+		<span>' . esc_html__( 'Integration API Base URL', 'myvideoroom' ) . '</span>
+		</td>
+		<td>
+		<input type="text" class="mvr-main-button-enabled myvideoroom-maintenance-setting"
+			id="' . esc_attr( self::SETTING_INTEGRATION_API_BASEURL ) . '" value="' . \get_option( self::SETTING_INTEGRATION_API_BASEURL ) . '">
+			<i class="myvideoroom-dashicons mvr-icons dashicons-editor-help" title="' . \esc_html__( 'Integration API Base URL', 'myvideoroom' ) . '"></i>
+		</td>';
+		\array_push( $input, $input_add );
+		return $input;
+	}
+
+	/**
+	 * Activate Functions for Integration Module.
+	 */
+	public function activate(): void {}
+
+	/**
+	 * De-Activate Functions for Integration Module.
+	 */
+	public function de_activate(): void {}
+
+}
---
+++ elemental/module/integration/coadjute/requests/class-createcompany.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ElementalPlugin\Module\Integration\Coadjute\Requests;
+
+use ElementalPlugin\Module\Integration\Coadjute\CoadjuteConnector;
+
+use Sammyjo20\Saloon\Constants\Saloon;
+use Sammyjo20\Saloon\Http\SaloonRequest;
+use Sammyjo20\Saloon\Traits\Plugins\HasFormParams;
+
+class CreateCompany extends SaloonRequest
+{
+    use HasFormParams;
+
+    /**
+     * The connector class.
+     *
+     * @var string|null
+     */
+    protected ?string $connector = CoadjuteConnector::class;
+
+    /**
+     * The HTTP verb the request will use.
+     *
+     * @var string|null
+     */
+    protected ?string $method = Saloon::POST;
+
+    public function __construct(
+        public string $nodeName
+    ){}
+
+    /**
+     * The endpoint of the request.
+     *
+     * @return string
+     */
+    public function defineEndpoint(): string
+    {
+        return '/companies';
+    }
+
+    public function defaultData(): array
+    {
+    	return [
+    		'node_name' => $this->nodeName,
+    	];
+    }
+}
