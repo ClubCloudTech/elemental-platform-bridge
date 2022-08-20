d7c2367109d56e9b68acc160e7fa1f67320f7c60
SidHarsh25 <harsh.25sj@gmail.com>
2022-08-20 14:03:47 +0530

    new NPM UI Fixes

--- elemental/module/membership/views/membership/view-adduser.php
+++ elemental/module/membership/views/membership/view-adduser.php
@@ -9,89 +9,88 @@
  */
 
 return function (
-    object $current_user
+	object $current_user
 ): string {
-    ob_start();
+	ob_start();
 
-    if (user_can($current_user, 'administrator')) {
-?>
-        <div id="primary" class="site-content">
-            <div id="content" role="main">
+	if ( user_can( $current_user, 'administrator' ) ) {
+		?>
+		<div id="primary" class="site-content">
+			<div id="content" role="main">
 
-                <div class="container editcontainer">
-                    <div class="row erow headrow">
-                        <div class="col-xs-12" id="formHeader">
-                            Add User
-                        </div>
-                    </div>
-                    <div class="row erow contentRow">
-                        <div class="col-xs-12" id="demoContainer">
-                            <form id="registrationForm" action="elemental_adduser_ajax" method="post" class="form-horizontal fv-form fv-form-bootstrap ajax" novalidate="novalidate">
-                                <button type="submit" class="fv-hidden-submit" style="display: none; width: 0px; height: 0px;"></button>
-
-
-                                <div class="form-group">
-                                    <label class="col-xs-3 control-label">UserName</label>
-                                    <div class="col-xs-8">
-                                        <input type="text" class="form-control" name="display_name" value="" />
-                                    </div>
-                                </div>
+				<div class="container editcontainer">
+					<div class="row erow headrow">
+						<div class="col-xs-12" id="formHeader">
+							Add User
+						</div>
+					</div>
+					<div class="row erow">
+						<div class="col-xs-12" id="demoContainer">
+							<form id="registrationForm" action="elemental_adduser_ajax" method="post" class="form-horizontal fv-form fv-form-bootstrap ajax" novalidate="novalidate">
+								<button type="submit" class="fv-hidden-submit" style="display: none; width: 0px; height: 0px;"></button>
 
 
-
-                                <div class="form-group">
-                                    <label class="col-xs-3 control-label">Email Address</label>
-                                    <div class="col-xs-8">
-                                        <input type="text" class="form-control" name="user_email" value="" />
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-xs-3 control-label">FirstName</label>
-                                    <div class="col-xs-8">
-                                        <input type="text" class="form-control" name="firstname" value="" />
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-xs-3 control-label">LastName</label>
-                                    <div class="col-xs-8">
-                                        <input type="text" class="form-control" name="lastname" value="" />
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-xs-3 control-label">Custom Capability</label>
-                                    <div class="col-xs-8">
-                                        <select class="form-control" name="user_role">
-                                    
-                                                <option value="0" selected>
-                                                    Subscriber
-                                                </option>
-                                                <option value="1">
-                                                   Other Role
-                                                </option>
-                                      
-                                        </select>
-                                    </div>
-                                </div>
-                                <input type="hidden" name="userid" value="<?php echo $current_user->ID; ?>">
-                                <div class="form-group">
-                                     <label class="col-xs-3 control-label"></label>
-                                    <div class="col-xs-8 col-xs-offset-3 lastButtonForm">
-                                        <button type="button" id="addUser" class="btn btn-primary profilebtn" name="add-user" value="add-user">Add User </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+								<div class="form-group">
+									<label class="col-xs-3 control-label">Username</label>
+									<div class="col-xs-8">
+										<input type="text" class="form-control" name="display_name" value="" />
+									</div>
+								</div>
 
 
-                </div><!-- #content -->
-            </div><!-- #primary -->
-    <?php
-    } else {
-        echo 'Login as admin ';
-    }
-    return ob_get_clean();
+
+								<div class="form-group">
+									<label class="col-xs-3 control-label">Email address</label>
+									<div class="col-xs-8">
+										<input type="text" class="form-control" name="user_email" value="" />
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="col-xs-3 control-label">Firstname</label>
+									<div class="col-xs-8">
+										<input type="text" class="form-control" name="firstname" value="" />
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="col-xs-3 control-label">Lastname</label>
+									<div class="col-xs-8">
+										<input type="text" class="form-control" name="lastname" value="" />
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="col-xs-3 control-label">Custom Capability</label>
+									<div class="col-xs-8">
+										<select class="form-control" name="user_role">
+									
+												<option value="0" selected>
+													Subscriber
+												</option>
+												<option value="1">
+												   Other Role
+												</option>
+									  
+										</select>
+									</div>
+								</div>
+								<input type="hidden" name="userid" value="<?php echo $current_user->ID; ?>">
+								<div class="form-group">
+									<div class="col-xs-9 col-xs-offset-3 lastButtonForm">
+										<button type="button" id="addUser" class="btn btn-primary profilebtn" name="add-user" value="add-user">Add User </button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+
+
+				</div><!-- #content -->
+			</div><!-- #primary -->
+		<?php
+	} else {
+		echo 'Login as admin ';
+	}
+	return ob_get_clean();
 };
