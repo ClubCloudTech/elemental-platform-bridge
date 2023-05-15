b836f479c1f58f65b14daed6e3a245b315c969f1
Fred Baumhardt <fred@acsd.uk>
2023-05-16 00:21:06 +0100

    picture fix

--- elemental/views/email/email-generic.php
+++ elemental/views/email/email-generic.php
@@ -17,7 +17,8 @@
 return function (
 	string $welcome_message,
 	string $body_message,
-	string $detail = null
+	string $detail = null,
+	string $image_header = null
 ): string {
 	ob_start();
 	?>
@@ -71,22 +72,19 @@ return function (
 						<td align="center" valign="middle" class="header"
 							style="word-wrap: break-word; -webkit-hyphens: auto; -moz-hyphens: auto; hyphens: auto; border-collapse: collapse !important; vertical-align: top;  -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; color: #444; font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; font-weight: normal; margin: 0; Margin: 0; text-align: left; font-size: 14px; line-height: 140%; background-color: #ffffff; padding: 60px 75px 45px 75px; border-right: 1px solid #ddd; border-bottom: 1px solid #ddd; border-left: 1px solid #ddd; border-top: 3px solid #809eb0;">
 							<?php
-							$image_header = wp_get_attachment_url( get_theme_mod( 'custom_logo' ) );
 							if ( $image_header ) {
 								?>
 							<img src="<?php echo esc_url( $image_header ); ?>" width="100" alt=""
 								style="outline: none; text-decoration: none; max-width: 100px; clear: both; -ms-interpolation-mode: bicubic; display: inline-block !important; width: 250px;">
-								<h4>
-								<?php
-								// phpcs:ignore --WordPress.Security.EscapeOutput.OutputNotEscaped - already escaped.
-								echo  $welcome_message;
-								?>
-							</h4>
-								
 								<?php
 							}
-
 							?>
+							<h4>
+								<?php
+							// phpcs:ignore --WordPress.Security.EscapeOutput.OutputNotEscaped - already escaped.
+							echo  $welcome_message;
+								?>
+							</h4>
 						</td>
 					</tr>
 					<!-- Content -->
